085e8d52d44fa5cf1c7e35d7b387149a89f18b63
serabiasin <ahmadalfiazdzikri@gmail.com>
2019-03-24 13:14:11 +0700

    Refactor code

--- socket_client.py
+++ socket_client.py
@@ -14,7 +14,9 @@ class socket_raspi(object):
         self.__dirDat = direktori_dataset
         self.__dirWav = direktori_wav
 
-    def doInference(self, arg):
+    def disconnect(self):
+        self.client_socket.close()
+    def doInference(self):
         pass
 
     def compress(self):
@@ -22,6 +24,12 @@ class socket_raspi(object):
         filename = "dataset"
         print("masuk")
         shutil.make_archive(filename,'zip',self.__dirDat)
+        namaZip=os.path.basename(self.__dirDat[:-1])+'.zip'
+        return namaZip
+
+    def doTrain(self):
+        namaZip=self.compress()
+        self.sendFile(namaZip)
 
 
     """
@@ -29,51 +37,53 @@ class socket_raspi(object):
     method ini kalau bisa flexibel, bisa buat ngirim zip,
     atau send wav(buat inference)
     """
-    def sendFile(self):
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    def sendFile(self,namaFile):
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         port = self.__port
         host = socket.gethostbyname(self.__host)
         print(host, port)
 
         try:
-            client_socket.connect((host, port))
+            self.client_socket.connect((host, port))
         except Exception as e:
             print("Koneksi error : ", e)
 
         # contoh implementasi send file ( untuk train model )
         # do compression data
-        self.compress()
+        # self.compress()
         # step 1 kirim ukuran file ke server_socket
-        filesize = os.path.getsize("dataset.zip")
+        filesize = os.path.getsize(namaFile)
         command = "send_file"
         dataSize = str(filesize)
-        client_socket.send(command.encode())
+        self.client_socket.send(command.encode())
         # step 2 tunggu balasan dari server
-        buffer = client_socket.recv(1024).decode()
+        buffer = self.client_socket.recv(1024).decode()
         # step 3 kirim file
         if buffer == 'ready':
             command = "ok"
-            client_socket.send(command.encode())
-            buffer = client_socket.recv(1024).decode()
+            self.client_socket.send(command.encode())
+            buffer = self.client_socket.recv(1024).decode()
             if buffer == 'do_it':
                 # simpan ukuran file (karena socket harus tau ukuran file nya dulu)
-                client_socket.send(dataSize.encode())
+                self.client_socket.send(dataSize.encode())
                 # step 5 send file
-                buffer = client_socket.recv(1024).decode()
+                buffer = self.client_socket.recv(1024).decode()
                 if buffer == 'whereisfile':
-                    with open('dataset.zip', 'rb') as berkas:
-                        client_socket.sendfile(berkas, 0)
+                    with open(namaFile, 'rb') as berkas:
+                        self.client_socket.sendfile(berkas, 0)
                     # step 6 DONE!!!!
                     berkas.close()
-                    client_socket.shutdown(socket.SHUT_WR)
+                    self.client_socket.shutdown(socket.SHUT_WR)
 
             print("Selesai")
-            client_socket.close()
 
+    def recvFile(self):
+        pass
 
 
         # contoh penggunaan class socket_raspi
         # ojek = socket_raspi(7000, "192.168.1.3")
 dir = "/home/ahmadalfi/Training/python/speech_processing/dataset/"
 ojek = socket_raspi(7000, "127.0.0.1", direktori_dataset=dir)
-ojek.sendFile()
+# ojek.sendFile()
+# ojek.doTrain()
